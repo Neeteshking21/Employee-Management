afe4b87c546860641c918f42e4ae31398c61306a
NeeteshKing21 <neeteshking21@gmail.com>
2021-08-31 08:25:57 +0530

    Code modification

--- Employee Maagemet/index2.js
+++ Employee Maagemet/index2.js
@@ -1,11 +1,11 @@
-document.getElementById("dropdown").style.display = "none"
-document.getElementById("quote1").style.display = "block"
+// document.getElementById("dropdown").style.display = "none"
+// document.getElementById("quote1").style.display = "block"
 document.getElementById("signInpage").style.display = "none"
 console.log("connected")
 function navbar() {
-    var x = document.getElementById('dropdown');
+    // var x = document.getElementById('dropdown');
     if (x.style.display === 'none') {
-      document.getElementById("quote1").style.display = "none"
+      // document.getElementById("quote1").style.display = "none"
       x.style.display = 'grid'
       setTimeout(()=>{
         document.getElementById("aware").style.cssText = "width:60vw;margin-left:30vw;"
@@ -14,7 +14,7 @@ function navbar() {
       
     } else {
       x.style.display = 'none';
-      document.getElementById("quote1").style.display = "block"
+      // document.getElementById("quote1").style.display = "block"
       document.getElementById("aware").style.cssText = "grid-area: aware;delay:0.5s;height: 100vh;width: 100%;background-color: rgb(44, 43, 43);"
       document.getElementById("awareH1").style.cssText="margin-top: 10vh;margin-left: 22vw;;"
 
